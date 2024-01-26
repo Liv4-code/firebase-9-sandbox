a42932f57585e5343ebe479d4353ebedc113ceab
livi <scholtz.ice@gmail.com>
2024-01-26 13:59:12 +0200

    Created sign up, sign in and logout feature

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ import {
     orderBy,
     serverTimestamp,
 } from "firebase/firestore";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signOut,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD2xO_a0xxYENTLq819Fi415wq3a-wyN3Q",
@@ -28,6 +34,7 @@ initializeApp(firebaseConfig);
 
 // init services
 const db = getFirestore();
+const auth = getAuth();
 
 // collection ref
 const colRef = collection(db, "books");
@@ -93,5 +100,52 @@ updateForm.addEventListener("submit", (e) => {
         })
         .catch((err) => {
             console.log(err);
+        });
+});
+
+// signing users up
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = signupForm.email.value;
+    const password = signupForm.password.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log("user created:", cred.user);
+            signupForm.reset();
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
+
+// logging in and out
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+    signOut(auth)
+        .then(() => {
+            console.log("user signed out");
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+});
+
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log("user logged in:", cred.user);
+            loginForm.reset();
+        })
+        .catch((err) => {
+            console.log(err.message);
         });
 });
